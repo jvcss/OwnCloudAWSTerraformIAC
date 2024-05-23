137ddf3a88514ed6ca8811f37908a457dac69353
jvcss <jvcs.mecatronica@gmail.com>
2024-05-23 18:00:08 -0300

    fix: misconfiguration

--- main.tf
+++ main.tf
@@ -25,25 +25,25 @@ data "aws_ami" "amzn-linux-2023-ami" {
 }
 
 # # definir regras de acesso
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Para segurança, restrinja ao seu IP
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]  # Para segurança, restrinja ao seu IP
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 # instalação da RSA para SSH
 resource "aws_key_pair" "deployer_key" {
