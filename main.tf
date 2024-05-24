af4c30b9f286817d3050995ff649ba75cc9f711d
jvcss <jvcs.mecatronica@gmail.com>
2024-05-24 02:07:00 -0300

    update memory usage owncloud

--- main.tf
+++ main.tf
@@ -143,32 +143,32 @@ resource "aws_instance" "app_server" {
 }
 
 # Automatiza Continuos Deploy com Docker Compose
-# resource "null_resource" "install_dependencies" {
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo yum install docker -y",
-#       "sudo service docker start",
-#       "sudo chkconfig docker on",
-#       "sudo usermod -aG docker ec2-user",
-#       "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
-#       "sudo chmod +x /usr/local/bin/docker-compose",
-#       "sudo yum install -y git",
-#       "git clone https://github.com/jvcss/OwnCloudAWSTerraformIAC.git ~/cliente",
-#       "cd ~/cliente",
-#       "sed -i 's/localhost/${aws_instance.app_server.private_ip}/g' ~/cliente/nginx/nginx.conf",
-#       "sed -i 's/localhost/${aws_instance.app_server.private_ip}/g' ~/cliente/docker-compose.yml"
-#     ]
-#     connection {
-#       # usamos endereço publico DNS
-#       host = aws_instance.app_server.public_dns
-#       # usuario da instancia
-#       user = "ec2-user"
-#       # caminho da chave SSH privada
-#       private_key = file("C:/Users/vitim/.ssh/novarsa.pem")
-#     }
-#   }
-#   depends_on = [aws_instance.app_server]
-# }
+resource "null_resource" "install_dependencies" {
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install docker -y",
+      "sudo service docker start",
+      "sudo chkconfig docker on",
+      "sudo usermod -aG docker ec2-user",
+      "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo yum install -y git",
+      "git clone https://github.com/jvcss/OwnCloudAWSTerraformIAC.git ~/cliente",
+      "cd ~/cliente",
+      "sed -i 's/localhost/${aws_instance.app_server.private_ip}/g' ~/cliente/nginx/nginx.conf",
+      "sed -i 's/localhost/${aws_instance.app_server.private_ip}/g' ~/cliente/docker-compose.yaml"
+    ]
+    connection {
+      # usamos endereço publico DNS
+      host = aws_instance.app_server.public_dns
+      # usuario da instancia
+      user = "ec2-user"
+      # caminho da chave SSH privada
+      private_key = file("C:/Users/vitim/.ssh/novarsa.pem")
+    }
+  }
+  depends_on = [aws_instance.app_server]
+}
 
 # # Reinicialização da instância porque é necessario após garantir as permissões de acesso do docker
 # resource "null_resource" "reboot_instance" {
