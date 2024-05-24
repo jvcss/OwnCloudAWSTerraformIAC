8b1c2f40e3d0d8aaca8c2d65eeb391113321a03f
jvcss <jvcs.mecatronica@gmail.com>
2024-05-24 02:20:23 -0300

    fix sed replace

--- main.tf
+++ main.tf
@@ -155,8 +155,7 @@ resource "null_resource" "install_dependencies" {
       "sudo yum install -y git",
       "git clone https://github.com/jvcss/OwnCloudAWSTerraformIAC.git ~/cliente",
       "cd ~/cliente",
-      "sed -i 's/localhost/${aws_instance.app_server.private_ip}/g' ~/cliente/nginx/nginx.conf",
-      "sed -i 's/localhost/${aws_instance.app_server.private_ip}/g' ~/cliente/docker-compose.yaml"
+      "sed -i 's/localhost/${aws_instance.app_server.public_dns}/g' ~/cliente/docker-compose.yaml"
     ]
     connection {
       # usamos endereço publico DNS
